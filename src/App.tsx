86b0cebffaca9632a67933a54de27be3e4aa8786
Pjaijai <paul6a24@hotmail.com>
2024-08-05 00:25:47 -0400

    data handling in app

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,26 @@ import { getBlocks } from './blocks';
 import { Header } from './components/Header/Header';
 import { InfoPanel } from './components/InfoPanel/InfoPanel';
 import { ImageGrid } from './components/ImageGrid/ImageGrid';
+import { useFetch } from './hooks/apis/use-fetch';
+import { useMemo } from 'react';
+import getImageBlocksHelper from './utils/helpers/get-image-blocks';
 
 export const App = () => {
+  const { data: block, isLoading, error } = useFetch(getBlocks);
+
+  const imageBlocks = useMemo(() => {
+    if (block) return getImageBlocksHelper(block);
+    return [];
+  }, [block, isLoading]);
+
   return (
     <Router>
       <Header />
       <main>
-        <ImageGrid />
-        <InfoPanel />
+        {error && <div>{error}</div>}
+        {isLoading && <p>Loading...</p>}
+        <ImageGrid imageBlocks={imageBlocks} />
+        <InfoPanel blocks={imageBlocks!} />
       </main>
     </Router>
   );
